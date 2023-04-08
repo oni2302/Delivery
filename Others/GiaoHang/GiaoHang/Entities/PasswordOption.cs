ce921149fe9423bf896ff2b3128d18eca8ddac76
nnp2302 <65790507+nnp2302@users.noreply.github.com>
2023-04-08 09:27:04 +0700

    temp
    temp

---
+++ Others/GiaoHang/GiaoHang/Entities/PasswordOption.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace GiaoHang
+{
+    public class PasswordOption
+    {
+        private static readonly byte[] key = { 2, 3, 0, 2 };
+        public static string Encrypt(string origin)
+        {
+            if (origin == null) throw new ArgumentNullException("origin");
+            //encrypt data
+            var data = Encoding.Unicode.GetBytes(origin);
+            byte[] encrypted = ProtectedData.Protect(data, key, DataProtectionScope.CurrentUser);
+
+            //return as base64 string
+            return Convert.ToBase64String(encrypted);
+        }
+
+        public static string Decrypt(string encrypt)
+        {
+            if (encrypt == null) throw new ArgumentNullException("encrypt");
+
+            //parse base64 string
+            byte[] data = Convert.FromBase64String(encrypt);
+
+            //decrypt data
+            byte[] decrypted = ProtectedData.Unprotect(data, key, DataProtectionScope.CurrentUser);
+            return Encoding.Unicode.GetString(decrypted);
+        }
+    }
+}
---
+++ Others/GiaoHang/GiaoHang/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GiaoHang.MvcApplication" Language="C#" %>
