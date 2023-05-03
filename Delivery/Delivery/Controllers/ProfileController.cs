32046b89636785e5e2d7fafed3b579727b02a94b
hiepbao <128014078+hiepbao@users.noreply.github.com>
2023-05-03 16:10:21 +0700

    Fix Profile & reset Pass

---
+++ Delivery/Delivery/Controllers/ProfileController.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Delivery.Controllers
+{
+    public class ProfileController : BaseController
+    {
+        // GET: Profile
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ProfileView(int? id)
+        {
+
+            var ListProduct = database.Profile_Get(id).SingleOrDefault();
+            if (ListProduct == null)
+            {
+
+                return HttpNotFound();
+            }
+            ViewBag.sua = ListProduct;
+
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult ProfileView(HttpPostedFileBase anhdaidien, DateTime ngaysinh)
+        {
+            var id = int.Parse(Request.Form["id"]);
+            var name = Request.Form["tennhanvien"];
+            var email = Request.Form["email"];
+            string sdt = (string)Request.Form["sodienthoai"];
+            byte[] buffer = null;
+            if (anhdaidien != null)
+            {
+                MemoryStream memoryStream = new MemoryStream();
+                anhdaidien.InputStream.CopyTo(memoryStream);
+                buffer = memoryStream.ToArray();
+            }
+
+
+            var result = database.Profile_Sua(id, name, ngaysinh, email, sdt);
+
+            return Redirect("~/Profile/ProfileView/" + id);
+        }
+
+        public ActionResult DoiMk(int? id)
+        {
+            var ListProduct = database.Profile_Get(id).SingleOrDefault();
+            if (ListProduct == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.sua = ListProduct;
+
+            return View();
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult DoiMk(int id, string MatKhau)
+        {
+            //var matkhau = Request.Form["MatKhau"];
+
+            var result = database.TaiKhoan_DoiMK(id, MatKhau).Single();
+
+            return Redirect("~/Profile/ProfileView/" + id);
+        }
+    }
+}
